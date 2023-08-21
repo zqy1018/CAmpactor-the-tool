0a2f41f875d83eb01112cfabfe801d9f773f9616
zqy1018 <zqy1018@sjtu.edu.cn>
2023-08-22 03:23:57 +0800

    removed redundant files

--- SamplingCA/riss-solver/riss/utils/Statistics-mt.cc
+++
@@ -1,9 +0,0 @@
-/********************************************************************************[Statistics-mt.cc]
-Copyright (c) 2012, Norbert Manthey, LGPL v2, see LICENSE
-**************************************************************************************************/
-
-#include "riss/utils/Statistics-mt.h"
-
-// global accessable statistics object
-
-Statistics statistics;
